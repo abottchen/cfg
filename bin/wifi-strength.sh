83604ba5764bd00e5306d79655bdd3be9ee28e53
Adam Bottchen <adam.bottchen@puppet.com>
2017-06-26 16:53:38 -0700

    Update wifi strength to pull first word when there is a space

--- bin/wifi-strength.sh
+++ bin/wifi-strength.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-printf "%s (%d%%)\n" $(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I |grep " SSID" |egrep -o "\S+$") $(bc -l <<< "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep "agrCtlRSSI:" |egrep -o "[-0-9]+$") + 100")
+printf "%s (%d%%)\n" $(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I |grep ' SSID' |perl -ne '($a) = $_ =~  /SSID: (.*?)$/; print $a') $(bc -l <<< "$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | grep "agrCtlRSSI:" |egrep -o "[-0-9]+$") + 100")
